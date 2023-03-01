b7e54a45ae5c5e9300eb5cfa3ea864b4f2e8febb
sowjanyaSadashiva <67077357+SowjanyaSadashivu@users.noreply.github.com>
2023-03-01 14:21:59 -0600

    Time: 23 ms, Memory: 13.8 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.py
+++ longest-common-prefix/longest-common-prefix.py
@@ -4,8 +4,6 @@ class Solution:
             return ""
         min_s = min(strs)
         max_s = max(strs)
-        #if not min_s:
-         #   return ""
         for i in range(len(min_s)):
             if max_s[i] != min_s[i]:
                 return max_s[:i]
